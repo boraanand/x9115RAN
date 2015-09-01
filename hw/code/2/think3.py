86fc5e30a4a325f830c7cac29e53ca2a41f633d2
Ravi Singh <rpsingh3@ncsu.edu>
2015-09-01 19:24:16 -0400

    update think3.py

--- hw/code/2/think3.py
+++ hw/code/2/think3.py
@@ -1,4 +1,7 @@
-"""Exercise 3.2 ( 3.1 returns expected errro)"""
+"""
+**Exercise 3.2 ( 3.1 returns expected errro)*****
+
+"""
 def repeat_lyrics():
     print_lyrics()
     print_lyrics()
@@ -9,7 +12,10 @@ def print_lyrics():
 
 repeat_lyrics()
 
-"Exercise 3.3"""
+"""
+*****Exercise 3.3**********************
+
+"""
 def right_justify(str):
     num_spaces = 70 - len(str)
     for i in range(num_spaces):
@@ -18,7 +24,9 @@ def right_justify(str):
 
 right_justify('allen') 
 
-"""Exercise 3.4"""
+
+"""
+******Exercise 3.4*********************"""
 def do_twice(f,num):
     f(num)
     f(num)
@@ -38,7 +46,11 @@ print ' '
 """Print four times"""
 do_four(print_twice,'spam')
 
-"""Exercise 3.5"""
+
+"""
+******Exercise 3.5***********************
+
+"""
 def horizontal():
     print '+','-','-','-','-',
 
@@ -70,6 +82,7 @@ def print_grid_base(col):
     print_horizontal_lines(col)
     print_vertical_lines(col)
     print_last_vertical_line(col)
+
 	
 "Function to print the grid(row,col)"
 def print_full_grid(row,col):
