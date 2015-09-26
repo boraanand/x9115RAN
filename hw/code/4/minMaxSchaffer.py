fad8ed102c1a2b2ce41e587950ec9cbca4a6842d
Anand Bora <bora.anand@yahoo.com>
2015-09-26 17:35:22 -0400

    init and first run

---
+++ hw/code/4/minMaxSchaffer.py
@@ -0,0 +1,25 @@
+import random as r
+
+def f1(x): return x ** 2
+
+def f2(x): return (x - 2) ** 2
+
+if __name__ == '__main__':
+    min = 100000
+    max = -100000
+    
+    for _ in range(100):
+        x = r.randint(-100000, 100000)
+        e = f1(x) + f2(x)
+        if min > e:
+            min = e
+        if max < e:
+            max = e
+        
+    
+    print 'Emin: ', min
+    print 'Emax: ', max
+    
+# Output #
+# Emin:  3044
+# Emax:  19993600514
--- hw/code/4/sa.py
+++ hw/code/4/sa.py
@@ -2,53 +2,59 @@ from __future__ import print_function, division
 import random as r
 import math
 
+Emin = 3044
+Emax = 19993600514
+
 class Schaffer():
-  def __init__(i, x, min_, max_): 
-    i.x = x
-    i.min_ = min_
-    i.max_ = max_
+    def __init__(i, x): 
+        i.x = x
+
+    def f1(i): return i.x ** 2
   
-  def f1(i): return i.x**2
-  
-  def f2(i): return (i.x-2)**2
+    def f2(i): return (i.x - 2) ** 2
 
-  def E(i):
-    return (i.f1() + i.f2() - i.min_)/ (i.max_ - i.min_)
+    def E(i):
+        return (i.f1() + i.f2() - Emin) / (Emax - Emin)
 
-  def neighbor(i):
-    return Schaffer(r.randrange(i.min_, i.max_), i.min_, i.max_)
+    def neighbor(i):  
+        randomNu = r.randint(-100000, 2 * i.x - (-100000))
+        return Schaffer(randomNu)
 
-def P(old,new,t):
-  return math.exp((old-new)/t)
+def P(old, new, t):
+    return math.exp((old - new) / t)
 
-def sa(x0, min_, max_):
-  s0 = Schaffer(x0, min_, max_)
-  s = s0; e = s.E()                 # Initial state, energy.
-  sb = s; eb = e                    # Initial "best" solution
-  k = 1                             # Energy evaluation count.
-  kmax = 1000
-  emax = -2.0
+def sa(x0):
+    s0 = Schaffer(x0)
+    s = s0; e = s.E()  # Initial state, energy.
+    sb = s; eb = e  # Initial "best" solution
+    k = 1  # Energy evaluation count.
+    kmax = 1000
+    emax = -2.0
 
-  while k < kmax and e > emax:      # While time remains & not good enough:
-    sn = s.neighbor()               # Pick some neighbor.
-    en = sn.E()                     # Compute its energy.
-    if en < eb:                     # Is this a new best?
-      sb = sn; eb = en              # Yes, save it.
-      print('!', end='')
+    while k < kmax :  # and e > emax While time remains & not good enough:
+        sn = s.neighbor()  # Pick some neighbor.
+        en = sn.E()  # Compute its energy.
+#         global Emin
+#         Emin = min(Emin, en)
+        if en < eb:  # Is this a new best?
+            sb = sn; eb = en  # Yes, save it.
+            print('!', end='')
 
-    if en < e:                      # Should we jump to better?
-      s = sn; e = en                # Yes!
-      print('+', end = '')
-    elif P(e, en, k/kmax) < r.random(): # Should we jump to worse?
-      s = sn; e = en                # Yes, change state.
-      print('?', end='')
+        if en < e:  # Should we jump to better?
+            s = sn; e = en  # Yes!
+            print('+', end='')
+        elif P(e, en, float(1 - (k / kmax))) < float((r.randint(0, 100)) / 100):  # Should we jump to worse?
+            s = sn; e = en  # Yes, change state.
+            print('?', end='')
     
-    print('.', end='')
-    k +=  1                         # One more evaluation done    
+        print('.', end='')
+        k += 1  # One more evaluation done    
     
-    if k % 50 == 0: print("\n",sb)
+        if k % 50 == 0: 
+            print (sb.E(), sb.x) 
+#           print("\n")
   
-  return sb
+    return sb
 
 if __name__ == '__main__':
-  sa(12, -1000, 1000)
+    sa(r.randint(-100000, 100000))
