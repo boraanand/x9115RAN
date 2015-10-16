db60bccb49415a783841f28e7cc7683d79fe72bd
Nirmesh <nirmesh.khandelwal@gmail.com>
2015-10-16 16:26:42 -0400

    Resolving #1

--- hw/code/3/README.md
+++ hw/code/3/README.md
@@ -1,5 +1,7 @@
 ### Poker hands exercise: 
 ##### 7 million runs were made and following graph shows co-relation to theoritical values.
+![Birthday Paradox](birthdat_paradox.png)
+![Employee](employee.png)
 ![graph](images/graph.png)
 ![table](images/prob.png)
 
--- hw/code/3/birthday_paradox.py
+++ hw/code/3/birthday_paradox.py
@@ -18,19 +18,23 @@ def has_duplicates(l):
             return True
     return False
 
-def same_birthday():
+def same_birthday(number_of_students, number_of_simulations):
     import random
     random.seed(245)
 
     #Run the experiment 100 times
     result = []
 
-    for i in range(100):
-        sample = [random.randint(1, 365) for x in range(23)]
+    for i in range(number_of_simulations):
+        sample = [random.randint(1, 365) for x in range(number_of_students)]
         
         if has_duplicates(sample): result.append(1)
         else: result.append(0)
-    probability = sum(result)/100
+    probability = sum(result)/number_of_simulations
     return probability
 
-
+if __name__ == '__main__':
+    print("Birthday Paradox")
+    print("Probability that two students have same birthday out of %d students: %f"% (23, same_birthday(23, 1000)) ) 
+    print("Probability that two students have same birthday out of %d students: %f"% (10, same_birthday(10, 1000)) ) 
+    print("Probability that two students have same birthday out of %d students: %f"% (20, same_birthday(20, 1000)) ) 
