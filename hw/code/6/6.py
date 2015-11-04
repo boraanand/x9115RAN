705d4a9cd913b9fd1acb1254754d69fa72dcfdac
Ravi Singh <rpsingh3@ncsu.edu>
2015-11-04 00:08:00 -0500

    Add Osyczka2 model class

--- hw/code/6/6.py
+++ hw/code/6/6.py
@@ -18,9 +18,6 @@ class Model(object):
     def getEnergy(i):
         return True
 
-#     def clone(i, other):
-#         i.dec = other.dec[:]
-
     def getobj(i):
         return []
 
@@ -62,8 +59,80 @@ class Schaffer(Model):
     
     def getEnergy(i):
         return i.normalize(sum(i.objs()))
+
+class Osyczka2(Model):
     
-# randomNu = r.randint(-100000, 2 * i.x - (-100000))  # Select neighbour a random in in a radius from current x to min.
+    def __init__(i):
+        i.dec = [[1, 0, 10]]
+	i.dec.append([1,0,10])
+	i.dec.append([2,1,5])
+	i.dec.append([1,0,6])
+	i.dec.append([2,1,5])
+	i.dec.append([1,0,10])
+        i.random()
+        i.Emin = -396.221369935
+        i.Emax = 146.314249947
+
+    # Check input constraints
+    def okx1x2(i):
+    	if i.dec[0][0] + i.dec[1][0] - 2 < 0 :
+            return False
+    	if 6 - i.dec[0][0] - i.dec[1][0] < 0:
+            return False
+    	if 2 - i.dec[1][0] + i.dec[0][0] < 0:
+            return False
+    	if 2 - i.dec[0][0] + 3 * i.dec[1][0] < 0:
+            return False
+    	return True
+
+    def okx3x4(i):
+    	if 4 - (i.dec[2][0] - 3) ** 2 - i.dec[3][0] < 0:
+            return False
+    	return True
+
+    def okx5x6(i):
+    	if (i.dec[4][0] - 3) ** 3 + i.dec[5][0] - 4 < 0:
+            return False
+    	return True
+
+    def ok(i):
+	return  i.okx1x2() and i.okx3x4() and i.okx5x6()
+
+    def random(i):
+        while not i.okx1x2():
+            i.dec[0][0] = r.uniform(i.dec[0][1], i.dec[0][2])
+            i.dec[1][0] = r.uniform(i.dec[1][1], i.dec[1][2])
+        while not i.okx3x4():
+            i.dec[2][0] = r.uniform(i.dec[2][1], i.dec[2][2])
+            i.dec[3][0] = r.uniform(i.dec[3][1], i.dec[3][2])
+        while not i.okx5x6():
+            i.dec[4][0] = r.uniform(i.dec[4][1], i.dec[4][2])
+            i.dec[5][0] = r.uniform(i.dec[5][1], i.dec[5][2])
+            
+    def normalize(i, e):
+
+        if i.Emin > e:
+            i.Emin = e
+        if i.Emax < e:
+            i.Emax = e
+
+        return (e - i.Emin) / (i.Emax - i.Emin)
+
+
+    # less than. Goal is to minimize. Return true if new < best.
+    def ltOrGt(i, Enew, Ebest):
+        if Enew > Ebest:
+            return True
+        return False
+        
+    def objs(i):
+        f1 = -1 * (25*(i.dec[0][0]-2) ** 2 + (i.dec[1][0] - 2) ** 2 + (i.dec[2][0] - 1) ** 2 * (i.dec[3][0] - 4) ** 2 + (i.dec[4][0] - 1) ** 2)
+        f2 = i.dec[0][0] ** 2 + i.dec[1][0] ** 2 + i.dec[2][0] ** 2 + i.dec[3][0] ** 2 + i.dec[4][0] ** 2 + i.dec[5][0] ** 2
+        return [f1, f2]
+    
+    def getEnergy(i):
+        return i.normalize(sum(i.objs()))
+    
 
 def neighbor(s, c, model):
     sn = deepcopy(s)
@@ -99,11 +168,20 @@ def sa(model):
         elif sb.ltOrGt(en,e): #Should we jump to better?
             s = deepcopy(sn); e = en  # Yes!
             print('+', end='')
-        elif P(e, en, float(50 * (k / kmax))) < float((r.randint(0, 100)) / 100):  # Should we jump to worse?
-            s = deepcopy(sn); e = en  # Yes, change state.
-            print('?', end='')
-        else:
-            print('.', end='')
+        else :
+            if sb.ltOrGt(1,2): #Looking for minimum energy?
+	        if P(e, en, float(50 * (k / kmax))) < float((r.randint(0, 100)) / 100):  # Should we jump to worse?
+            	    s = deepcopy(sn); e = en  # Yes, change state.
+                    print('?', end='')
+                else:
+                    print('.', end='')
+            else: #Looking for maximum energy?
+	        if P(1-e, 1-en, float(50 * (k / kmax))) < float((r.randint(0, 100)) / 100):  # Should we jump to worse?
+            	    s = deepcopy(sn); e = en  # Yes, change state.
+                    print('?', end='')
+                else:
+                    print('.', end='')
+
         k += 1  # One more evaluation done    
     
         if k % 50 == 0: 
@@ -112,7 +190,7 @@ def sa(model):
     print ('\nBest Energy = ', eb, ' at x = ', zip(*sb.dec)[0])
 
 if __name__ == '__main__':
-    for model in [Schaffer]:
+    for model in [Schaffer,Osyczka2]:
         for optimizer in [sa]:
 #             print(optimizer.__name__, +" : " + model.__name__)
             optimizer(model)
