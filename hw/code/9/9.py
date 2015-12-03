da4400a53b81ec674fa8f85288ec64194da6fa90
Nirmesh <nirmesh.khandelwal@gmail.com>
2015-12-03 01:08:26 -0500

    Modularising the code

--- hw/code/9/9.py
+++ hw/code/9/9.py
@@ -1,10 +1,10 @@
 from __future__ import print_function, division
-from models import *
-from algorithms import *
+from ..source.models import *
+from ..source.algorithms import *
 
 if __name__ == '__main__':
    
-    for model in [DTLZ_3]:#, DTLZ_1, DTLZ_3, DTLZ_5]:
+    for model in [DTLZ_3, DTLZ_1, DTLZ_3, DTLZ_5]:
         for optimizer in [ga]:
             optimizer().optimize(model(n=10, m=2))
         print ("\n\n")
